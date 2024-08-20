fe87d9c0e1edb8e110743eb7dc1fe729ac1b9f5c
XuCinu <489938468@qq.com>
2024-08-20 20:28:03 +0800

    无

--- 201703/2.java
+++ 201703/2.java
@@ -1,5 +1,60 @@
+
+import java.util.Scanner;
+
 public class Main{
+    static class Node {
+        int num;
+        Node s;
+        Node x;
+
+        public Node(Node s, Node x){
+            this.s = s;
+            this.x = x;
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        Node head = new Node(null, null), x = head;
+        for (int i = 1; i <= n; i++){
+            x.x = new Node(x, null);
+            x = x.x;
+            x.num = i;
+        }
+        for (int i = 0; i < m; i++){
+            int p = sc.nextInt(), q = sc.nextInt();
+            x = head;
+            while(x.num!=p){
+                x = x.x;
+            }
+            Node temp = x;
+            if(x.x!=null){
+                x = x.x;
+                x.s = x.s.s;
+            }
+            if(x.s!=null){
+                x = x.s;
+                x.x = x.x.x;
+            }
+            while(q<0){
+                x = x.s;
+                q++;
+            }
+            while(q>0){
+                x = x.x;
+                q--;
+            }
+            temp.x = x.x;
+            temp.s = x;
+            x.x = temp;
+            x = x.x.x;
+            if(x!=null){
+                x.s = temp;
+            }
+        }
+        x = head.x;
+        for (int i = 0; i < n; i++, x = x.x){
+            System.out.print(x.num + " ");
+        }
     }
 }
--- 201809/2.java
+++ 201809/2.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), index = 0, sum = 0;
+        boolean[] h = new boolean[1000001], w = new boolean[1000001];
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt(), b = sc.nextInt();
+            for (int j = a; j < b; j++) {
+                h[j] = true;
+            }
+            index = Math.max(index, b);
+        }
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt(), b = sc.nextInt();
+            for (int j = a; j < b; j++) {
+                w[j] = true;
+            }
+            index = Math.max(index, b);
+        }
+        for(int i = 0; i < index; i++){
+            if(h[i] && w[i]){
+                sum++;
+            }
+        }
+        System.out.println(sum);
     }
 }
